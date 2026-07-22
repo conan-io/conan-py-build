751bd7072baeb55cb0c26e5f8f2fb893d261613e
Carlos Zoido <mrgalleta@gmail.com>
2026-07-22 11:53:22 +0200

    improve cpython test_package

--- recipes/cpython/test_package/conanfile.py
+++ recipes/cpython/test_package/conanfile.py
@@ -1,7 +1,7 @@
-from six import StringIO
+from io import StringIO
+
 from conan import ConanFile
 from conan.tools.build import can_run
-import re
 
 
 class TestPackageConan(ConanFile):
@@ -13,13 +13,15 @@ class TestPackageConan(ConanFile):
     def test(self):
         if not can_run(self):
             return
-        output = StringIO()
+
         python = "python" if self.settings.os == "Windows" else "python3"
+
+        output = StringIO()
         self.run(f"{python} --version", output, env="conanrun")
-        output_str = str(output.getvalue())
-        self.output.info("Installed version: {}".format(output_str))
-        tokens = re.split('[@#]', self.tested_reference_str)
-        require_version = tokens[0].split("/", 1)[1]
-        self.output.info("Expected version: {}".format(require_version))
-        assert_python_version = "Python %s" % require_version
-        assert assert_python_version in output_str
+        version_output = output.getvalue().strip()
+        self.output.info(f"Installed version: {version_output}")
+
+        expected_version = self.dependencies[self.tested_reference_str].ref.version
+        expected_output = f"Python {expected_version}"
+        assert expected_output in version_output, \
+            f"Expected '{expected_output}' in output, got '{version_output}'"
